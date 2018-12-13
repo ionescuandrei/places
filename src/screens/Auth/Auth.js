3201df1fcfcae1a006da9b3fd3fc4403dc26e61b
Ionescu Andrei <ionescuandrei86@yahoo.com>
2018-12-13 11:46:02 +0200

    added responive

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Button, StyleSheet, ImageBackground} from 'react-native';
+import {View, Button, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 import startMainTab from "../MainTabs/startTab";
 import DefaultTextInput from '../../UI/defaultTextInput/DefaultTextInput';
 import HeadingText from '../../UI/headingText/HeadingText';
@@ -8,20 +8,42 @@ import imagBackground from '../../assets/background.jpg';
 import ButtonWithbackground from '../../UI/buttonWithBackground/ButtonWithBackground';
 
 class AuthScreen extends React.Component{
+  state={
+    viewMode:Dimensions.get("window").height>500? "portrait":"landscape"
+  }
+  constructor(props){
+    super(props);
+    Dimensions.addEventListener("change", this.updateStyles);  
+    
+  }
+  componentWillUnmount(){
+    Dimensions.removeEventListener("change", this.updateStyles)
+  }
+  updateStyles=(dims)=>{
+    this.setState({viewMode:dims.window.height>500? "portrait":"landscape"})  
+  }
   loginHandler=() => {
     startMainTab();
   }
   render(){
+    let headingText=null;
+    if(this.state.viewMode === "portrait"){
+      headingText=(
+        <MainText><HeadingText>Please login</HeadingText></MainText>
+      )
+    }
     return(
       <ImageBackground source={imagBackground} style={styles.imageBackground}>
       <View style={styles.container}>      
-        <MainText><HeadingText>Please login</HeadingText></MainText>
+        {headingText}
         <ButtonWithbackground color="#29aaf4" onPress={()=>{alert("Hello!")}}>Switch to login</ButtonWithbackground>       
         {/* <Button title="Switch to login"/> */}
         <View style={styles.inputContainer}>
         <DefaultTextInput placeholder="Your e-mail adress" style={styles.inputText}/>
-        <DefaultTextInput placeholder="Set password" style={styles.inputText}/>
-        <DefaultTextInput placeholder="Confirm password" style={styles.inputText}/>
+        <View style={this.state.viewMode === "portrait"?styles.portraitPasswordContainer:styles.landscapePasswordContainer}>
+        <View style={this.state.viewMode === "portrait"?styles.portraitPasswordWrapper:styles.landscapePasswordWrapper}><DefaultTextInput placeholder="Confirm password" style={styles.inputText}/></View>
+        <View style={this.state.viewMode === "portrait"?styles.portraitPasswordWrapper:styles.landscapePasswordWrapper}><DefaultTextInput placeholder="Set password" style={styles.inputText}/></View>
+        </View>
         </View>
         <ButtonWithbackground  color="#29aaf4" onPress={this.loginHandler}>Submit</ButtonWithbackground>       
       </View>
@@ -46,6 +68,19 @@ const styles = StyleSheet.create({
     width:"100%",
     height:"100%",
     flex: 1
+  },
+  portraitPasswordContainer:{
+    flexDirection:"column"     
+  },
+  landscapePasswordContainer:{
+    flexDirection: "row",
+    justifyContent:"space-between"
+  },
+  portraitPasswordWrapper:{
+    width:"100%"    
+  },
+  landscapePasswordWrapper:{
+    width:"45%"    
   }
 
 })
--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 export default class SideDrawer extends Component {
   
   render() {
     return (
       <View style={[style.container,{width:Dimensions.get('window').width*0.8}]}>
-        <Text> textInComponent </Text>
+        <TouchableOpacity >
+              <View style={style.drawerItem}>  
+                  <Icon name="ios-log-out" size={30} color="#bbb" style={style.drawerItemIcon}/>   
+                  <Text> Sign out </Text>                       
+                </View>
+         </TouchableOpacity>
       </View>
+      
     );
   }
 }
@@ -16,5 +22,14 @@ const style = StyleSheet.create({
     paddingTop: 20,
     backgroundColor:'white',
     flex: 1    
+  },
+  drawerItem:{
+    flexDirection: 'row',
+    alignItems:"center",
+    padding: 5,
+    backgroundColor:"#eee"
+  },
+  drawerItemIcon:{
+    marginRight: 10
   }
 })
